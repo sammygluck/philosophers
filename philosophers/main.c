71855a8ff1579e170ce655b296e2c0e79058c9b6
Sammy Gluck <infogls@yahoo.com>
2023-07-23 20:43:13 +0200

    testing thread

--- philosophers/main.c
+++ philosophers/main.c
@@ -1,7 +1,22 @@
 #include "philosophers.h"
 
+int i = 2;
+
+void *test(void *parameter)
+{
+    printf("Value is %i\n", *(int *)p);
+
+    pthread_exit(&i);
+}
+
 int main(void)
 {
-    printf("hell9");
-    return (0);
+    pthread_t id;
+    int j;
+
+    j = 1;
+    pthread_create(&id, NULL, test, &j);
+    pthread_join(id, (void **)&ptr);
+    printf("Value received by parent from chilld: ");
+    printf("%i\n", *ptr);
 }
--- philosophers/philosophers.h
+++ philosophers/philosophers.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
 
 
 #endif
