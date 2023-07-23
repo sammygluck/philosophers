f91fc01b6a771ad28451a185d9a5fb1a383cdc71
Sammy Gluck <infogls@yahoo.com>
2023-07-23 21:47:37 +0200

    minor change

--- philosophers/main.c
+++ philosophers/main.c
@@ -9,15 +9,17 @@ void *test(void *p)
     pthread_exit(&i);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
     pthread_t id;
     int j;
     int *ptr;
 
     j = 1;
+    (void) argc;
+    (void) argv;
     pthread_create(&id, NULL, test, &j);
     pthread_join(id, (void **)&ptr);
-    printf("Value received by parent from chilld: ");
+    printf("Value received by parent from child: ");
     printf("%i\n", *ptr);
 }
